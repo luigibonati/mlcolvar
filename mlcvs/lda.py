86f35dcaa678b7673e22985f5b68894485f462ea
Luigi Bonati <lui.bonati@gmail.com>
2021-09-23 00:48:44 +0200

    added deeplda

--- mlcvs/lda.py
+++ mlcvs/lda.py
@@ -2,6 +2,8 @@
 
 import torch
 import numpy as np
+from .nn import NeuralNetworkCV
+from torch.utils.data import Dataset, DataLoader
 
 class LinearDiscriminant:
     """
@@ -30,10 +32,14 @@ class LinearDiscriminant:
         Fit LDA and project data 
     get_params()
         Return saved parameters
+    set_features_names(names)
+        Set features names
     plumed_input()
         Generate PLUMED input file
     """
+
     def __init__(self):
+        super().__init__()
         # initialize attributes
         self.evals_ = None
         self.evecs_ = None
@@ -41,7 +47,9 @@ class LinearDiscriminant:
         self.S_w_ = None
         self.d_ = None
         self.features_names_ = None
-        self.lambda_ = 1e-6
+
+        # Regularization
+        self.sw_reg = 1e-6
 
         # Initialize device and dtype
         self.dtype_ = torch.float32
@@ -96,7 +104,7 @@ class LinearDiscriminant:
         S_b = S_t - S_w
 
         # Regularize S_w
-        S_w = S_w + self.lambda_ * torch.diag(torch.Tensor().new_ones((d), device = self.device_, dtype = self.dtype_))
+        S_w = S_w + self.sw_reg * torch.diag(torch.Tensor().new_ones((d), device = self.device_, dtype = self.dtype_))
 
         # -- Generalized eigenvalue problem: S_b * v_i = lambda_i * Sw * v_i --
 
@@ -214,19 +222,21 @@ class LinearDiscriminant:
         out['S_within']=self.S_w_
         return out
     
-    def set_sw_regularization(self,reg):
+    def set_regularization(self,sw_reg):
         """
         Set regularization for within-scatter matrix.
 
-        .. math:: S_w = S_w + \mathtt{reg}\ \mathbf{1}. 
-
         Parameters
         ----------
-        reg : float
+        sw_reg : float
             Regularization value.
+
+        Notes
+        -----
+        .. math:: S_w = S_w + \mathtt{sw_reg}\ \mathbf{1}. 
         
         """
-        self.lambda_ = reg
+        self.sw_reg = 0.05
 
     def set_features_names(self,names):
         """
@@ -276,6 +286,299 @@ class LinearDiscriminant:
         return out 
 
 
-#if __name__ == "__main__":
+class DeepLDA(NeuralNetworkCV,LinearDiscriminant):
+    """
+    Neural network based discriminant CV. 
+    Perform a non-linear featurization of the inputs with a neural-network and optimize it as to maximize Fisher's discriminant ratio.
 
+    Attributes
+    ----------
+    d_ : int
+        Number of classes
+    evals_ : torch.Tensor
+        LDA eigenvalues
+    evecs_ : torch.Tensor
+        LDA eignvectors
+    S_b_ : torch.Tensor
+        Between scatter matrix
+    S_w_ : torch.Tensor
+        Within scatter matrix
+        
+    Methods
+    -------
+    forward(x)
+        Compute DeepLDA CV
+    set_regularization(sw_reg,lorentzian_reg)
+        Set magnitudes of regularizations.
+    train()
+        Train NN
+    evaluate_dataset(x,label)
+        Fit LDA and project data 
+    get_params()
+        Return saved parameters
+    set_features_names(names) #TODO Add
+        Set features names
+    plumed_input() #TODO add
+        Generate PLUMED input file
+    """
 
+    def __init__(self, layers, activation = 'relu'):
+        super().__init__(layers=layers,activation=activation)
+
+        #lorentzian reg
+        self.lorentzian_reg = 0 
+        self.sw_reg = 0.05
+        
+        #training logs
+        self.epochs = 0
+        self.loss_train = []
+        self.loss_valid = []
+        self.log_header = True
+
+        #output
+        self.output_hidden = False
+           
+    def get_params(self):
+        """
+        Return attached parameters.
+        
+        Returns
+        -------
+        out : dictionary
+            Parameters
+        """
+        out = dict()
+        out['device']=self.device_
+        out['Sw regularization']=self.sw_reg
+        out['Lorentzian regularization']=self.lorentzian_reg
+        out['Early Stopping']=True if self.earlystopping_ is not None else False
+        out['Input standardization']=self.normIn
+        out['# epochs']=self.epochs
+        return out
+
+    def forward(self, x: torch.tensor) -> (torch.tensor):
+        """
+        Compute model output. First propagate input through the NN and then apply LDA.
+
+        Parameters
+        ----------
+        x : torch.tensor
+            input data
+
+        Returns
+        -------
+        z : torch.tensor
+            CVs
+
+        See Also
+        --------
+        forward_nn : compute forward pass of NN module
+
+        """
+        z = self.forward_nn(x)
+        if (not self.output_hidden ):
+            z = self.transform(z)
+        return z
+
+    def regularization_lorentzian(self, x):
+        """
+        Compute lorentzian regularization on NN outputs. 
+
+        Parameters
+        ----------
+        x : float
+            input data
+        """
+        reg_loss = x.pow(2).sum().div( x.size(0) )
+        reg_loss_lor = - self.lorentzian_reg / (1+(reg_loss-1).pow(2))
+        return reg_loss_lor
+    
+    def loss_function(self,H,y,save_params=False):
+        """
+        Loss function for the DeepLDA CV. Correspond to maximizing the eigenvalue(s) of LDA plus a regularization on the NN outputs.
+
+        Parameters
+        ----------
+        H : torch.tensor
+            NN output
+        y : torch.tensor
+            labels
+        save_params: bool
+            save the eigenvalues/vectors of LDA into the model
+
+        Returns
+        -------
+        loss : torch.tensor
+            loss function
+        """
+        eigvals = self.LDA(H, y, save_params)
+        loss = - eigvals[0] # TODO GENERALIZE TO MULTICLASS
+        if self.lorentzian_reg > 0:
+            loss += self.regularization_lorentzian(H)
+        return loss
+        
+    def train_epoch(self, loader):
+        """
+        Function for training an epoch.
+
+        Parameters
+        ----------
+        loader: DataLoader
+            training set
+        """
+        for data in loader:
+            # =================get data===================
+            X,y = data[0].float().to(self.device_),data[1].long().to(self.device_)
+            # =================forward====================
+            H = self.forward_nn(X)
+            # =================lda loss===================
+            loss = self.loss_function(H,y,save_params=False)
+            # =================backprop===================
+            self.opt_.zero_grad()
+            loss.backward()
+            self.opt_.step()
+        # ===================log======================
+        self.epochs +=1
+        
+    def evaluate_dataset(self, data, save_params=False):
+        """
+        Loss function for the DeepLDA CV. Correspond to maximizing the eigenvalue(s) of LDA plus a regularization on the NN outputs.
+
+        Parameters
+        ----------
+        data : array-like (data,labels)
+            validation dataset
+        save_params: bool
+            save the eigenvalues/vectors of LDA into the model
+
+        Returns
+        -------
+        loss : torch.tensor
+            loss function
+        """   
+        with torch.no_grad():
+            X,y = data[0].float().to(self.device_),data[1].long().to(self.device_)
+            H  = self.forward_nn(X)
+            loss = self.loss_function(H,y,save_params)
+        return loss
+
+    def set_regularization(self, sw_reg = 0.02, lorentzian_reg = None):
+        """
+        Set magnitude of regularizations for the training:
+        - add identity matrix multiplied by `sw_reg` to within scatter S_w.
+        - add lorentzian regularization to NN outputs with magnitude `lorentzian_reg`
+
+        If `lorentzian_reg` is None, set it equal to `2./sw_reg`.
+
+        Parameters
+        ----------
+        sw_reg : float
+            Regularization value for S_w.
+        lorentzian_reg: float
+            Regularization for lorentzian on NN outputs.
+
+        Notes
+        -----
+        These regularizations are described in [1]_.
+        .. [1] Luigi Bonati, Valerio Rizzi, and Michele Parrinello, J. Phys. Chem. Lett. 11, 2998-3004 (2020).
+
+        - S_w
+        .. math:: S_w = S_w + \mathtt{sw_reg}\ \mathbf{1}. 
+        
+        - Lorentzian
+        TODO Add equation
+        
+        """
+        self.sw_reg = sw_reg
+        if lorentzian_reg is None:
+            self.lorentzian_reg = 2./sw_reg
+        else:
+            self.lorentzian_reg = lorentzian_reg
+            
+    def train(self, train_data, valid_data=None, standardize_inputs=True, batch_size=-1, nepochs=1000, log_every=1, info=False):
+        """
+        Train Deep-LDA CVs.
+
+        Parameters
+        ----------
+        train_data: DataLoader
+            training set
+        valid_data: list of torch.tensors (X:input, y:labels)
+            validation set
+        standardize_inputs: bool
+            whether to standardize input data
+        batch_size: bool, optional
+            number of points per batch (default = -1 --> single batch)
+        nepochs: int, optional
+            number of epochs (default = 1000)
+        log_every: int, optional
+            frequency of log (default = 1)
+        print_info: bool, optional
+            print debug info (default = False)
+        """
+
+        #check optimizer
+        if self.opt_ is None:
+            self.default_optimizer()
+        
+        #create dataloader
+        train_dataset = ColvarDataset(train_data)
+        if batch_size==-1:
+            batch_size=len(train_data[0])
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        #standardize inputs
+        if standardize_inputs:
+            self.standardize_inputs(train_data[0])
+            
+        #print info
+        if info:
+            self.print_info()
+        
+        #train
+        for ep in range(nepochs):
+            self.train_epoch(train_loader)
+            
+            loss_train = self.evaluate_dataset(train_data,save_params=True)
+            loss_valid = self.evaluate_dataset(valid_data)
+            self.loss_train.append(loss_train)
+            self.loss_valid.append(loss_valid)
+            
+            #earlystopping
+            if self.earlystopping_ is not None:
+                self.earlystopping_(loss_valid,self.parameters)
+                
+            #log
+            if ((ep+1) % log_every == 0) or ( self.earlystopping_.early_stop ):
+                self.print_log({'Epoch':ep+1,'Train Loss':loss_train,'Valid Loss':loss_valid},
+                               spacing=[6,12,12],decimals=2)
+                
+            #check whether to stop
+            if (self.earlystopping_ is not None) and (self.earlystopping_.early_stop):
+                self.parameters = self.earlystopping_.best_model
+                break
+
+class ColvarDataset(Dataset):
+    """
+    COLVAR dataset
+    
+    """
+
+    def __init__(self, colvar_list):
+        self.nstates = len( colvar_list )
+        self.colvar = colvar_list
+        
+    def __len__(self):
+        return len(self.colvar[0])
+
+    def __getitem__(self, idx):
+        x = ()
+        for i in range(self.nstates):
+            x += (self.colvar[i][idx],)
+        return x
+    
+#useful for cycling over the test dataset if it is smaller than the training set
+def cycle(iterable):
+    while True:
+        for x in iterable:
+            yield x
