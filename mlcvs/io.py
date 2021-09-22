bd5f71fdbd6a5f62ce6a56dea0f921b67d36dd3d
Luigi Bonati <lui.bonati@gmail.com>
2021-09-22 09:52:02 +0200

    added io module

---
+++ mlcvs/io.py
@@ -0,0 +1,28 @@
+"""Input/output functions."""
+
+import pandas as pd
+
+def colvar_to_pandas(filename='COLVAR',folder='./',sep=' '):
+    """
+        Load a PLUMED colvar file and save it to a dataframe.
+        
+        Parameters
+        ----------
+        filename : string, optional
+            Collective variables file
+        folder : string, optional
+            Folder
+        sep: string, optional
+            Fields separator
+            
+        Returns
+        -------
+        df : DataFrame
+            Collective variables dataframe
+    """
+    skip_rows=1
+    headers = pd.read_csv(folder+filename,sep=' ',skipinitialspace=True, nrows=0).columns[2:]  
+    df = pd.read_csv(folder+filename,sep=sep,skipinitialspace=True,
+                         header=None,skiprows=range(skip_rows),names=headers,comment='#')
+    
+    return df
