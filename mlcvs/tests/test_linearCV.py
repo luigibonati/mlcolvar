89862b06dc8529d62c11682de61de5562e12b647
Luigi Bonati <lui.bonati@gmail.com>
2021-09-24 01:58:20 +0200

    added parametrize mark to pytest

--- mlcvs/tests/test_linearCV.py
+++ mlcvs/tests/test_linearCV.py
@@ -3,34 +3,47 @@ Unit and regression test for the linearCV class.
 """
 
 # Import package, test suite, and other packages as needed
+import pytest
 import torch
 from mlcvs.models import LinearCV
 
-def test_linearCV():
+# set global variables 
+dtype = torch.float32
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+@pytest.mark.parametrize("n_input", [1, 2]) 
+@pytest.mark.parametrize("dev", ['cpu','cuda'])
+def test_linearCV(n_input,dev):
     """Define linear combination."""
 
+    # Set device
+    device = torch.device(dev)
+    if not torch.cuda.is_available():
+        pytest.skip("cuda not available")
+
     # Define model
-    n_input = 2
-    cv = LinearCV(n_features=n_input)
+    #n_input = 2
+    cv = LinearCV(n_features=n_input, device=device)
 
     # Define inputs
-    x = torch.ones(n_input)*1
+    x = torch.ones(n_input).to(device)
 
     # Propagate with default values
     y = cv.transform(x)
-    expected_y = torch.tensor(2.)
-    # ASSERT
-    assert y == expected_y 
-
+    expected_y = torch.tensor([1. for _ in range(n_input)]).to(device)
+    print(y,expected_y)
+    # ASSERT arrays
+    assert torch.equal(y,expected_y)
+        
     # Change parameters
     cv.set_weights(0.5*torch.ones(n_input))
     cv.set_offset(0.5*torch.ones(n_input))
 
     # Propagate (call method)
     y2 = cv(x)
-    expected_y2 = torch.tensor(0.5)
+    expected_y2 = torch.tensor(0.25*n_input).to(device)
     # ASSERT
-    assert y2 == expected_y2
+    assert torch.equal(y2,expected_y2)
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
