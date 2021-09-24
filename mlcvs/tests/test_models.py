63f758469d1d76fd08029bbde43d02c9e58bd9aa
Luigi Bonati <lui.bonati@gmail.com>
2021-09-24 02:45:44 +0200

    added test_nn

--- mlcvs/tests/test_models.py
+++ mlcvs/tests/test_models.py
@@ -5,7 +5,7 @@ Unit and regression test for the linearCV class.
 # Import package, test suite, and other packages as needed
 import pytest
 import torch
-from mlcvs.models import LinearCV
+from mlcvs.models import LinearCV, NeuralNetworkCV
 
 # set global variables 
 dtype = torch.float32
@@ -13,8 +13,8 @@ dtype = torch.float32
 
 @pytest.mark.parametrize("n_input", [1, 2]) 
 @pytest.mark.parametrize("dev", ['cpu','cuda'])
-def test_linearCV(n_input,dev):
-    """Define linear combination."""
+def test_LinearCV(n_input,dev):
+    """Test Linear CV class."""
 
     # Set device
     device = torch.device(dev)
@@ -31,7 +31,6 @@ def test_linearCV(n_input,dev):
     # Propagate with default values
     y = cv.transform(x)
     expected_y = torch.tensor([1. for _ in range(n_input)]).to(device)
-    print(y,expected_y)
     # ASSERT arrays
     assert torch.equal(y,expected_y)
         
@@ -44,7 +43,45 @@ def test_linearCV(n_input,dev):
     expected_y2 = torch.tensor(0.25*n_input).to(device)
     # ASSERT
     assert torch.equal(y2,expected_y2)
+ 
+@pytest.mark.parametrize("n_input", [1, 2]) 
+@pytest.mark.parametrize("dev", ['cpu','cuda'])
+def test_NeuralNetworkCV(n_input,dev):
+    """Test NN CV class."""
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    test_linearCV()
+    # Set device
+    device = torch.device(dev)
+    if not torch.cuda.is_available():
+        pytest.skip("cuda not available")
+
+    # Parameters
+    n_input = 1
+    n_output = 2 
+
+    # Define model
+    net = NeuralNetworkCV(layers=[n_input,5,n_output], activation = 'relu')
+    net.to(device)
+
+    # Define inputs
+    x = torch.ones(n_input).to(device)
+
+    # Forward 
+    y = net(x)
+
+    # Assert
+    expected_y_shape = torch.rand(n_output).shape
+    assert y.shape == expected_y_shape
+
+    # Project output along single direction
+    w = torch.ones(n_output).to(device)
+    b = torch.ones(n_output).to(device)
+    net.set_params( {'w' : w } )
+    net.set_params( {'b' : b } )
+
+    # Forward 2
+    y2 = net(x)
+    
+    # Assert
+    expected_y2_shape = torch.tensor(1).shape
+    assert y2.shape == expected_y2_shape
+
