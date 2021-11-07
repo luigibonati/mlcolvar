6238c26ce178ce7ddb97cbc9d21b920bbd7ff029
Luigi Bonati <lui.bonati@gmail.com>
2021-11-07 18:16:47 +0100

    added Timelagged dataset

--- mlcvs/utils/data.py
+++ mlcvs/utils/data.py
@@ -1,8 +1,12 @@
-"""Dataloader."""
+"""Datasets."""
 
-__all__ = ["LabeledDataset"]
+import torch 
+import numpy as np
+
+__all__ = ["LabeledDataset","TimeLaggedDataset"]
 
 from torch.utils.data import Dataset
+from bisect import bisect_left
 
 class LabeledDataset(Dataset):
     """
@@ -31,4 +35,118 @@ class LabeledDataset(Dataset):
         x = (self.colvar[idx], self.labels[idx])
         return x
 
+def closest_idx_torch(array, value):
+        '''
+        Find index of the element of 'array' which is closest to 'value'.
+        The array is first converted to a np.array in case of a tensor.
+        Note: it does always round to the lowest one.
 
+        Parameters:
+            array (tensor/np.array)
+            value (float)
+
+        Returns:
+            pos (int): index of the closest value in array
+        '''
+        if type(array) is np.ndarray:
+            pos = bisect_left(array, value)
+        else:
+            pos = bisect_left(array.numpy(), value)
+        if pos == 0:
+            return 0
+        elif pos == len(array):
+            return -1
+        else:
+            return pos-1
+
+def find_time_lagged_configurations(x,t,lag):
+    '''
+    Searches for all the pairs which are distant 'lag' in time, and returns the weights associated to lag=lag as well as the weights for lag=0.
+
+    Parameters:
+        x (tensor): array whose columns are the descriptors and rows the time evolution
+        time (tensor): array with the simulation time
+        lag (float): lag-time
+
+    Returns:
+        x_t (tensor): array of descriptors at time t
+        x_lag (tensor): array of descriptors at time t+lag
+        w_t (tensor): weights at time t
+        w_lag (tensor): weights at time t+lag
+    '''
+    #define lists
+    x_t = []
+    x_lag = []
+    w_t = []
+    w_lag = []
+    #find maximum time idx
+    idx_end = closest_idx_torch(t,t[-1]-lag)
+    start_j = 0
+    #loop over time array and find pairs which are far away by lag
+    for i in range(idx_end):
+        stop_condition = lag+t[i+1]
+        n_j = 0
+        for j in range(i,len(t)):
+            if ( t[j] < stop_condition ) and (t[j+1]>t[i]+lag):
+                x_t.append(x[i])
+                x_lag.append(x[j])
+                deltaTau=min(t[i+1]+lag,t[j+1]) - max(t[i]+lag,t[j])
+                w_lag.append(deltaTau)
+                if n_j == 0: #assign j as the starting point for the next loop
+                    start_j = j
+                n_j +=1
+            elif t[j] > stop_condition:
+                break
+        for k in range(n_j):
+            w_t.append((t[i+1]-t[i])/float(n_j))
+
+    x_t = torch.stack(x_t) if type(x) == torch.Tensor else torch.Tensor(x_t)
+    x_lag = torch.stack(x_lag) if type(x) == torch.Tensor else torch.Tensor(x_lag)
+    
+    w_t = torch.Tensor(w_t)
+    w_lag = torch.Tensor(w_lag)
+
+    return x_t,x_lag,w_t,w_lag
+
+class TimeLaggedDataset(Dataset):
+    """
+    Dataset with pairs of configurations at time t and t+lag.
+    """
+
+    def __init__(self, X, t = None, lag_time = 10, logweights = None):
+        """
+        Create dataset of time-lagged configurations, with optional weights to reweight the trajectory.
+
+        TODO DOC
+        """
+
+        # define time if not given
+        if t is None:
+            t = np.arange(0,len(X))
+
+        # rescale time with log-weights
+        if logweights is not None:
+            # compute time increment in simulation time t
+            dt = np.round(t[1]-t[0],3)
+            # compute instantaneus time increment in rescaled time t'
+            d_tprime = np.copy(np.exp(logweights)*dt)
+            #calculate cumulative time tau
+            tprime = np.cumsum(d_tprime)
+        else:
+            tprime = t
+
+        # find pairs of configurations separated by lag_time
+        data = find_time_lagged_configurations(X, tprime,lag=lag_time)
+        
+        # save data and weights
+        self.x_t = data[0]
+        self.x_lag = data[1]
+        self.w_t = data[2]
+        self.w_lag = data[3]
+
+    def __len__(self):
+        return len(self.x_t)
+
+    def __getitem__(self, idx):
+        x = (self.x_t[idx], self.x_lag[idx], self.w_t[idx], self.w_lag[idx])
+        return x
