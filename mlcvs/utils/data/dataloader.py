615969c2ba46ad573f0b253fcd2786ace18326e8
Luigi Bonati <lui.bonati@gmail.com>
2023-01-17 12:28:02 +0100

    add data utils

---
+++ mlcvs/utils/data/dataloader.py
@@ -0,0 +1,101 @@
+"""Datasets."""
+
+import torch 
+from torch.utils.data import TensorDataset
+
+__all__ = ["FastTensorDataLoader"]
+
+from torch.utils.data import Dataset,Subset
+
+class FastTensorDataLoader:
+    """
+    A DataLoader-like object for a set of tensors.
+    
+    It is much faster than TensorDataset + DataLoader because dataloader grabs individual indices of the dataset and calls cat (slow).
+
+    Notes
+    =====
+
+    Adapted from https://discuss.pytorch.org/t/dataloader-much-slower-than-manual-batching/27014/6. 
+
+    """
+    def __init__(self, tensors, batch_size=0, shuffle=False):
+        """Initialize a FastTensorDataLoader.
+
+        Parameters
+        ----------
+        tensors : list of tensors or torch.Dataset or torch.Subset or list of torch.Subset object containing a tensors object
+            tensors to store. Must have the same length @ dim 0.
+        batch_size : int, optional
+            batch size, by default 0 (==single batch)
+        shuffle : bool, optional
+            if True, shuffle the data *in-place* whenever an
+            iterator is created out of this object, by default False
+
+        Returns
+        -------
+        FastTensorDataLoader
+            dataloader-like object
+
+        """
+
+        # check input type
+        if isinstance(tensors,Subset):
+            tensors = [ tensors.dataset.tensors[i][tensors.indices] for i in range(len(tensors.dataset.tensors)) ]
+        elif isinstance(tensors,Dataset):
+            tensors = [ tensors.tensors[i] for i in range(len(tensors.tensors)) ]
+        # check for input type list of Subset, and create a list of tensors
+        elif (isinstance(tensors,list) and isinstance(tensors[0],Subset) ):
+            new_tensors = []
+            tensor = torch.Tensor()
+            for j in range( len( tensors[0].dataset.tensors ) ):
+                for i in range( len( tensors ) ):
+                    if i == 0:
+                        tensor = tensors[i].dataset.tensors[j][tensors[i].indices]
+                    else:    
+                        tensor = torch.cat( (tensor, tensors[i].dataset.tensors[j][tensors[i].indices]), 0 )
+                new_tensors.append(tensor)
+            tensors = new_tensors
+
+        assert all(t.shape[0] == tensors[0].shape[0] for t in tensors)
+        self.tensors = tensors
+
+        self.dataset_len = self.tensors[0].shape[0]
+        self.batch_size = batch_size if batch_size > 0 else self.dataset_len
+        self.shuffle = shuffle
+
+        # Calculate # batches
+        n_batches, remainder = divmod(self.dataset_len, self.batch_size)
+        if remainder > 0:
+            n_batches += 1
+    def __iter__(self):
+        if self.shuffle:
+            self.indices = torch.randperm(self.dataset_len)
+        else:
+            self.indices = None
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i >= self.dataset_len:
+            raise StopIteration
+        if self.indices is not None:
+            indices = self.indices[self.i:self.i+self.batch_size]
+            batch = tuple(torch.index_select(t, 0, indices) for t in self.tensors)
+        else:
+            batch = tuple(t[self.i:self.i+self.batch_size] for t in self.tensors)
+        self.i += self.batch_size
+        return batch
+
+    def __len__(self):
+        return self.n_batches
+
+def test_fasttensordataloader(): 
+    X = torch.arange(1,11).unsqueeze(1)
+    y = X**2
+    dataloader = FastTensorDataLoader([X,y],batch_size=2)
+    print(next(iter(dataloader)))
+
+if __name__ == "__main__":
+    test_fasttensordataloader()
+    
---
+++ mlcvs/utils/data/datamodule.py
@@ -0,0 +1,82 @@
+import torch
+import pytorch_lightning as pl
+from torch.utils.data import TensorDataset, random_split
+from . import FastTensorDataLoader
+from typing import Union
+
+__all__ = ["TensorDataModule"]
+
+class TensorDataModule(pl.LightningDataModule):
+    """Lightning DataModule constructed for TensorDataset(s)."""
+    def __init__(self, dataset: TensorDataset, lengths=[0.8,0.2,0], batch_size: Union[int,list] = 32, shuffle : Union[bool,list] =  False,  generator : torch.Generator = None):
+        """Create a DataModule derived from TensorDataset, which returns train/valid/test dataloaders.
+
+        For the batch_size and shuffle parameters either a single value or a list-type of values (with same size as lenghts) can be provided.
+
+        Parameters
+        ----------
+        dataset : TensorDataset
+            Train dataset
+        lengths : list, optional
+            Lenghts of the training/validation/test datasets , by default [0.8,0.2]
+        batch_size : int or list, optional
+            Batch size, by default 32
+        shuffle : Union[bool,list], optional
+            whether to shuffle the batches from the dataloader, by default False
+        generator : torch.Generator, optional
+            set random generator for reproducibility, by default None
+        """
+        super().__init__()
+        self.dataset = dataset
+        self.lengths = lengths
+        if isinstance(batch_size,int):
+            self.batch_size = [batch_size for _ in lengths ]
+        else:
+            self.batch_size = batch_size
+        if isinstance(shuffle,bool):
+            self.shuffle = [shuffle for _ in lengths ]
+        else:
+            self.shuffle = shuffle
+        self.generator = None
+
+    def setup(self, stage: str):
+        self.dataset_splits = random_split(self.dataset, self.lengths, generator=self.generator)
+
+    def train_dataloader(self):
+        """Return training dataloader."""
+        return FastTensorDataLoader(self.dataset_splits[0], batch_size=self.batch_size[0],shuffle=self.shuffle[0])
+
+    def val_dataloader(self):
+        """Return validation dataloader."""
+        return FastTensorDataLoader(self.dataset_splits[1], batch_size=self.batch_size[1],shuffle=self.shuffle[1])
+
+    def test_dataloader(self):
+        """Return test dataloader."""
+        if len(self.lengths) >= 3:
+            return FastTensorDataLoader(self.dataset_splits[2], batch_size=self.batch_size[2],shuffle=self.shuffle[2])
+        else: 
+            raise ValueError('Test dataset not available, you need to pass three lenghts to datamodule.')  
+
+    def predict_dataloader(self):
+        raise NotImplementedError
+
+    def teardown(self, stage: str):
+        pass 
+
+def test_tensordatamodule():
+    torch.manual_seed(42)
+    X = torch.randn((100,2))
+    y = X.square()
+    dataset = TensorDataset(X,y)
+
+    datamodule = TensorDataModule(dataset,lengths=[0.75,0.2,0.05],batch_size=25)
+    datamodule.setup('fit')
+    loader = datamodule.train_dataloader()
+    for data in loader:
+        x_i, y_i = data
+        print(x_i.shape, y_i.shape)
+    datamodule.val_dataloader()
+    datamodule.test_dataloader()
+
+if __name__ == "__main__":
+    test_tensordatamodule() 
