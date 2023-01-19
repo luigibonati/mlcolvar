a836e8fe16df8e70d351c22489bb2f01475b4fb8
EnricoTrizio <enricotrizio@gmail.com>
2023-01-19 12:14:32 +0100

    Added tensor input to loss

--- mlcvs/core/loss/tda_loss.py
+++ mlcvs/core/loss/tda_loss.py
@@ -5,8 +5,8 @@ __all__ = ["TDA_loss"]
 def TDA_loss(H : torch.tensor,
             labels : torch.tensor,
             n_states : int,
-            target_centers : list,
-            target_sigmas : list,
+            target_centers : list or torch.tensor,
+            target_sigmas : list or torch.tensor,
             alfa : float = 1,
             beta : float = 100) -> torch.tensor:
     """
@@ -20,10 +20,10 @@ def TDA_loss(H : torch.tensor,
         Labels of the dataset
     n_states : int
         Number of states in the target
-    target_centers : list
+    target_centers : list or torch.tensor
         Centers of the Gaussian targets
         Shape: (n_states, n_cvs)
-    target_sigmas : list
+    target_sigmas : list or torch.tensor
         Standard deviations of the Gaussian targets
         Shape: (n_states, n_cvs)
     alfa : float, optional
@@ -36,9 +36,11 @@ def TDA_loss(H : torch.tensor,
     torch.tensor
         Total loss, centers loss, sigmas loss
     """
+    if type(target_centers) is list:
+        target_centers = torch.tensor(target_centers)
+    if type(target_sigmas) is list:
+        target_sigmas = torch.tensor(target_sigmas)
     
-    target_centers = torch.tensor(target_centers)
-    target_sigmas = torch.tensor(target_sigmas)
     loss_centers = torch.zeros_like(target_centers, dtype = torch.float32)
     loss_sigmas = torch.zeros_like(target_sigmas, dtype = torch.float32)
     
@@ -46,15 +48,15 @@ def TDA_loss(H : torch.tensor,
         # check which elements belong to class i
         H_red = H[torch.nonzero(labels == i).view(-1)]
 
-        # compute mean over the class i
+        # compute mean and standard deviation over the class i
         mu = torch.mean(H_red, 0)
-        # compute standard deviation over class i
         sigma = torch.std(H_red, 0)
 
-        # compute loss function contribute for class i
+        # compute loss function contributes for class i
         loss_centers[i] = alfa*(mu - target_centers[i]).pow(2)
         loss_sigmas[i] = beta*(sigma - target_sigmas[i]).pow(2)
-       
+
+    # get total model loss   
     loss_centers = torch.sum(loss_centers)
     loss_sigmas = torch.sum(loss_sigmas) 
     loss = loss_centers + loss_sigmas  
