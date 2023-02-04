4e3c00b4b177471a35851979a70da03477ed2d11
Luigi Bonati <lui.bonati@gmail.com>
2023-02-04 12:20:42 +0100

    removed duplicated func in normalization

--- mlcvs/core/transform/normalization.py
+++ mlcvs/core/transform/normalization.py
@@ -154,7 +154,7 @@ class Normalization(torch.nn.Module):
 
         The standardization mode can be either 'mean_std' (remove by the mean and divide by the standard deviation) or 'min_max' (scale and shift the range of values such that all inputs are between -1 and 1).
 
-        By default the normalization is computed on a running estimate based on each epoch. Disable it to save the parameters at the batch level. 
+        By default the normalization is computed on a running estimate along each epoch. Disable it to save the parameters at the batch level. 
 
         Parameters
         ----------
@@ -187,7 +187,7 @@ class Normalization(torch.nn.Module):
         self.epoch_wise = epoch_wise
 
     def extra_repr(self) -> str:
-        return f"in_features={self.in_features}, mode={self.mode}, epoch_wise={self.epoch_wise}"
+        return f"in_features={self.in_features}, out_features={self.out_features}, mode={self.mode}, epoch_wise={self.epoch_wise}"
 
     def reset_stats(self) -> None:
         """Reset running statistics."""
@@ -206,28 +206,6 @@ class Normalization(torch.nn.Module):
     def on_train_epoch_end(self) -> None:
         if self.epoch_wise:
             self.save_stats()
-
-    def batch_reshape(t: torch.Tensor, size : torch.Size) -> (torch.Tensor):
-        """Return value reshaped according to size. 
-        In case of batch expand unsqueeze and expand along the first dimension.
-        For single inputs just pass:
-
-        Parameters
-        ----------
-            Mean and range 
-
-        """
-        if len(size) == 1:
-            pass
-        if len(size) == 2:
-            batch_size = size[0]
-            x_size = size[1]
-            t = t.unsqueeze(0).expand(batch_size, x_size)
-        else:
-            raise ValueError(
-                f"Input tensor must of shape (n_features) or (n_batch,n_features), not {size}."
-            )
-        return t
 
     def forward(self, x: torch.Tensor) -> (torch.Tensor):
         """
