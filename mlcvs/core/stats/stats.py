1d1db4f7e140e9c1f0cf6a60b25d55a84dc6a2f0
EnricoTrizio <enricotrizio@gmail.com>
2023-03-01 14:20:22 +0100

    Added minimal doc

--- mlcvs/core/stats/stats.py
+++ mlcvs/core/stats/stats.py
@@ -8,16 +8,16 @@ class Stats(torch.nn.Module):
     To implement a new stats override the compute and forward methods. 
     The parameters of the stats should be set either in the initialization or via the setup_from_datamodule function.
     """
-    def setup_from_datamodule(self, datamodule):
-        """
-        Initialize parameters based on pytorch lighting datamodule.
-        """
-        pass
-
     def compute(self, X: torch.Tensor):
+        """
+        Compute the parameters of the estimator
+        """
         raise NotImplementedError
 
     def forward(self, X: torch.Tensor):
+        """
+        Apply estimator
+        """
         raise NotImplementedError
 
     def teardown(self):
