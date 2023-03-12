f3011f20c99cb282f4541c76f558a09368e17511
Luigi Bonati <lui.bonati@gmail.com>
2023-03-12 13:59:45 +0100

    created nn utils with activation

--- mlcvs/core/nn/feedforward.py
+++ mlcvs/core/nn/feedforward.py
@@ -1,38 +1,8 @@
 import torch 
 import pytorch_lightning as pl
+from mlcvs.core.nn.utils import get_activation, parse_nn_options
 
 __all__ = ["FeedForward"]
-
-def get_activation_from_string(activation : str):
-    activ = None
-    if activation == "relu":
-        activ = torch.nn.ReLU(True)
-    elif activation == "elu":
-        activ = torch.nn.ELU(True)
-    elif activation == "tanh":
-        activ = torch.nn.Tanh()
-    elif activation == "linear":
-        print("WARNING: no activation selected")
-    elif activation == None:
-        pass
-    else:
-        raise ValueError(
-            f"Unknown activation: {activation}. options: 'relu','elu','tanh','linear'. "
-        )
-    return activ
-
-def parse_nn_options( options : str, n_layers : int ):
-    # If an iterable is given cheeck that its length matches the number of NN layers
-    if hasattr(options, '__iter__') and not isinstance(options, str):
-        if len(options) != n_layers:
-            raise ValueError(f'Length of options: {options} ({len(options)} should be equal to number of layers ({n_layers})).')
-        options_list = options
-    # if a single value is given, repeat options to all layers but for the output one
-    else: 
-        options_list = [ options for _ in range(n_layers-1) ]
-        options_list.append(None)
-    
-    return options_list
 
 class FeedForward(pl.LightningModule):
 
@@ -82,7 +52,7 @@ class FeedForward(pl.LightningModule):
             activ, drop, norm = activation_list[i], dropout_list[i], batchnorm_list[i]
 
             if activ is not None:
-                modules.append(get_activation_from_string(activ))
+                modules.append(get_activation(activ))
 
             if drop is not None:
                 modules.append(torch.nn.Dropout(p=drop,inplace=True))
@@ -95,9 +65,9 @@ class FeedForward(pl.LightningModule):
         self.in_features   = layers[0]
         self.out_features  = layers[-1]
 
-    def extra_repr(self) -> str:
-        repr = f"in_features={self.in_features}, out_features={self.out_features}"
-        return repr
+    #def extra_repr(self) -> str:
+    #    repr = f"in_features={self.in_features}, out_features={self.out_features}"
+    #    return repr
 
     def forward(self, x: torch.tensor) -> (torch.tensor):
         return self.nn(x)
