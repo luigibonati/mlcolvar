e2b5e5999440c4c85651d345a4d2934544b5fde4
Luigi Bonati <lui.bonati@gmail.com>
2023-04-05 11:32:41 +0200

    extend load_dataframe to support urls

--- mlcvs/tests/test_utils_io.py
+++ mlcvs/tests/test_utils_io.py
@@ -1,6 +1,7 @@
 import pytest
 
-from mlcvs.utils.io import test_datasetFromFile
+from mlcvs.utils.io import test_loadDataframe,test_datasetFromFile
 
 if __name__ == "__main__":
+    test_loadDataframe()
     test_datasetFromFile() 
