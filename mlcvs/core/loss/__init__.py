add340c473ee62ffebf22d391ea0d96c4f89844e
Andrea Rizzi <andrrizzi@gmail.com>
2023-04-06 16:21:23 +0200

    Add loss classes to __init__

--- mlcvs/core/loss/__init__.py
+++ mlcvs/core/loss/__init__.py
@@ -1,9 +1,9 @@
 __all__ = ["mse_loss","tda_loss","elbo_gaussians_loss","reduce_eigenvalues_loss","autocorrelation_loss","fisher_discriminant_loss"]
 
-from .mse import mse_loss
-from .tda_loss import tda_loss
-from .eigvals import reduce_eigenvalues_loss
-from .elbo import elbo_gaussians_loss
-from .autocorrelation import autocorrelation_loss
-from .fisher import fisher_discriminant_loss
+from .mse import MSELoss, mse_loss
+from .tda_loss import TDALoss, tda_loss
+from .eigvals import ReduceEigenvaluesLoss, reduce_eigenvalues_loss
+from .elbo import ELBOGaussiansLoss, elbo_gaussians_loss
+from .autocorrelation import AutocorrelationLoss, autocorrelation_loss
+from .fisher import FisherDiscriminantLoss, fisher_discriminant_loss
 
