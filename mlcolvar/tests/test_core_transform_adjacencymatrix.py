503dacac0031d47a9e0043ee544e2c7f603287fd
EnricoTrizio <enricotrizio@gmail.com>
2023-05-16 15:23:56 +0200

    Rebuilt transform branch due to conflicts on deleted files with main

---
+++ mlcolvar/tests/test_core_transform_adjacencymatrix.py
@@ -0,0 +1,6 @@
+import pytest
+
+from mlcolvar.core.transform.eigs_adjacency_matrix import test_eigs_of_adj_matrix
+
+if __name__ == "__main__":
+    test_eigs_of_adj_matrix()
---
+++ mlcolvar/tests/test_core_transform_pairwisedistances.py
@@ -0,0 +1,6 @@
+import pytest
+
+from mlcolvar.core.transform.pairwise_distances import test_pairwise_distances
+
+if __name__ == "__main__":
+    test_pairwise_distances()
