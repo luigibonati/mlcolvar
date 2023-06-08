7436fc47a0d43ff2bde4aeb365dfc85fbe09d7ae
EnricoTrizio <enricotrizio@gmail.com>
2023-06-08 13:56:26 +0200

    Fixed output shape of rdf transform

--- mlcolvar/core/transform/radial_distribution_function.py
+++ mlcolvar/core/transform/radial_distribution_function.py
@@ -91,9 +91,9 @@ class RDF(Transform):
         x = self.compute_rdf(x)
         if self.peak_integral is not None:
             if self.integral_baseline:
-                integral = torch.sum((x[:, self.peak_lower_bound:self.peak_upper_bound] - 1) * self.bins_size, dim = 1)
+                integral = torch.sum((x[:, self.peak_lower_bound:self.peak_upper_bound] - 1) * self.bins_size, dim = 1, keepdim=True)
             else:
-                integral = torch.sum((x[:, self.peak_lower_bound:self.peak_upper_bound]) * self.bins_size, dim = 1)
+                integral = torch.sum((x[:, self.peak_lower_bound:self.peak_upper_bound]) * self.bins_size, dim = 1, keepdim=True)
             return integral
         else:
             return x
