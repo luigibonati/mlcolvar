3707076be84d393bbf925bb7e1ab841b9611f923
EnricoTrizio <enricotrizio@gmail.com>
2024-03-12 17:17:07 +0100

    Fixed part of transform utils tests

--- mlcolvar/tests/test_core_transform_utils.py
+++ mlcolvar/tests/test_core_transform_utils.py
@@ -1,6 +1,8 @@
 import pytest
 
-from mlcolvar.core.transform.utils import test_statistics
+from mlcolvar.core.transform.utils import test_statistics, test_adjacency_matrix, test_applycutoff
 
 if __name__ == "__main__":
     test_statistics()
+    test_adjacency_matrix()
+    test_applycutoff()
