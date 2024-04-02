0c095c4789fd800473a2d3f23ecb41c05433005f
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-04-02 03:32:18 +0200

    Modified file layouts.

--- mlcolvar/graph/__init__.py
+++ mlcolvar/graph/__init__.py
@@ -1,10 +1,6 @@
 """The graph neural network module."""
 
-__all__ = []
-
-# Add imports here
 from . import utils
 from . import core 
 from . import data 
 from . import cvs
-from . import io
--- mlcolvar/graph/utils/__init__.py
+++ mlcolvar/graph/utils/__init__.py
@@ -1,2 +1,3 @@
+from . import io
 from . import atomic
 from . import torch_tools
