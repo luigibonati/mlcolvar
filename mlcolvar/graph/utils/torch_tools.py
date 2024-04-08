4d85d665ef6711e8e2212cbace6104745ae4ea61
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-04-08 15:26:18 +0200

    Added edge encoding function.

--- mlcolvar/graph/utils/torch_tools.py
+++ mlcolvar/graph/utils/torch_tools.py
@@ -1,11 +1,16 @@
 import torch
+from typing import Tuple
 
 """
 The helper functions for torch. This module is taken from MACE directly:
 https://github.com/ACEsuit/mace/blob/main/mace/tools/torch_tools.py
 """
 
-__all__ = ['to_one_hot']
+__all__ = [
+    'to_one_hot',
+    'set_default_dtype',
+    'get_edge_vectors_and_lengths'
+]
 
 
 def to_one_hot(indices: torch.Tensor, n_classes: int) -> torch.Tensor:
@@ -14,7 +19,7 @@ def to_one_hot(indices: torch.Tensor, n_classes: int) -> torch.Tensor:
 
     Parameters
     ----------
-    indices: torch.tensor (shape: [N, 1])
+    indices: torch.Tensor (shape: [N, 1])
         Node incices.
     n_classes: int
         Number of classes.
@@ -31,6 +36,47 @@ def to_one_hot(indices: torch.Tensor, n_classes: int) -> torch.Tensor:
     oh.scatter_(dim=-1, index=indices, value=1)
 
     return oh.view(*shape)
+
+
+def get_edge_vectors_and_lengths(
+    positions: torch.Tensor,  # [n_nodes, 3]
+    edge_index: torch.Tensor,  # [2, n_edges]
+    shifts: torch.Tensor,  # [n_edges, 3]
+    normalize: bool = True,
+    eps: float = 1e-9,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Calculate edge vectors and lengths by indices and shift vectors.
+
+    Parameters
+    ----------
+    position: torch.Tensor (shape: [n_atoms, 3])
+        The position vector.
+    edge_index: torch.Tensor (shape: [2, n_edges])
+        The edge indices.
+    shifts: torch.Tensor (shape: [n_edges, 3])
+        The shift vector.
+    normalize: bool
+        If return the normalized distance vectors.
+    eps: float
+        The tolerance of zero-length vectors.
+
+    Returns
+    -------
+    vectors: torch.Tensor (shape: [n_edges, 3])
+        The distance vectors.
+    lengths: torch.Tensor (shape: [n_edges, 1])
+        The edge lengths.
+    """
+    sender = edge_index[0]
+    receiver = edge_index[1]
+    vectors = positions[receiver] - positions[sender] + shifts  # [n_edges, 3]
+    lengths = torch.linalg.norm(vectors, dim=-1, keepdim=True)  # [n_edges, 1]
+    if normalize:
+        vectors_normed = vectors / (lengths + eps)
+        return vectors_normed, lengths
+
+    return vectors, lengths
 
 
 def set_default_dtype(dtype: str) -> None:
@@ -65,6 +111,86 @@ def test_to_one_hot() -> None:
     ).all()
 
 
+def test_get_edge_vectors_and_lengths() -> None:
+    dtype = torch.get_default_dtype()
+    torch.set_default_dtype(torch.float64)
+
+    data = dict()
+    data['positions'] = torch.tensor(
+        [[0.0, 0.0, 0.0], [0.07, 0.07, 0.0], [0.07, -0.07, 0.0]],
+        dtype=torch.float64
+    )
+    data['edge_index'] = torch.tensor(
+        [[0, 0, 1, 1, 2, 2], [2, 1, 0, 2, 1, 0]]
+    )
+    data['shifts'] = torch.tensor([
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.2, 0.0],
+        [0.0, -0.2, 0.0],
+        [0.0, 0.0, 0.0],
+    ])
+
+    vectors, distances = get_edge_vectors_and_lengths(**data, normalize=False)
+    assert (
+        torch.abs(
+            vectors
+            - torch.tensor([
+                [0.0700, -0.0700, 0.0000],
+                [0.0700,  0.0700, 0.0000],
+                [-0.070, -0.0700, 0.0000],
+                [0.0000,  0.0600, 0.0000],
+                [0.0000, -0.0600, 0.0000],
+                [-0.070,  0.0700, 0.0000]
+            ])
+        ) < 1E-12
+    ).all()
+    assert (
+        torch.abs(
+            distances
+            - torch.tensor([
+                [0.09899494936611666],
+                [0.09899494936611666],
+                [0.09899494936611666],
+                [0.06000000000000000],
+                [0.06000000000000000],
+                [0.09899494936611666],
+            ])
+        ) < 1E-12
+    ).all()
+
+    vectors, distances = get_edge_vectors_and_lengths(**data, normalize=True)
+    assert (
+        torch.abs(
+            vectors
+            - torch.tensor([
+                [0.70710677404369050, -0.7071067740436905, 0.0],
+                [0.70710677404369050,  0.7071067740436905, 0.0],
+                [-0.7071067740436905, -0.7071067740436905, 0.0],
+                [0.00000000000000000,  0.9999999833333336, 0.0],
+                [0.00000000000000000, -0.9999999833333336, 0.0],
+                [-0.7071067740436905,  0.7071067740436905, 0.0],
+            ])
+        ) < 1E-12
+    ).all()
+    assert (
+        torch.abs(
+            distances
+            - torch.tensor([
+                [0.09899494936611666],
+                [0.09899494936611666],
+                [0.09899494936611666],
+                [0.06000000000000000],
+                [0.06000000000000000],
+                [0.09899494936611666],
+            ])
+        ) < 1E-12
+    ).all()
+
+    torch.set_default_dtype(dtype)
+
+
 def test_set_default_dtype() -> None:
     set_default_dtype('float64')
     t = torch.Tensor([1.0])
@@ -85,4 +211,5 @@ def test_set_default_dtype() -> None:
 
 if __name__ == '__main__':
     test_to_one_hot()
+    test_get_edge_vectors_and_lengths()
     test_set_default_dtype()
