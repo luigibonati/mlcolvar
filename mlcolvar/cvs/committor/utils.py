b1846c41ccf804f33ab4afa9fab68fa7f3e2b620
EnricoTrizio <enricotrizio@gmail.com>
2024-05-09 14:34:58 +0200

    Improved weights function

--- mlcolvar/cvs/committor/utils.py
+++ mlcolvar/cvs/committor/utils.py
@@ -1,63 +1,58 @@
 import torch
 import numpy as np
+from typing import List
 
 __all__ = ["compute_committor_weights", "initialize_committor_masses"]
 
-def compute_committor_weights(
-    dataframe, 
-    dataset, 
-    beta : float
-    ):
-    """Utils to compute the appropriate weights for the training set for the learning of committor function.
-    Compute the weights of training data based on the bias and the iteration they belong to.
-    This function is meant to be used on a dataframe and dataset generated using the mlcolvar.utils.io.create_dataset_from_files function
+def compute_committor_weights(dataset, 
+                              bias: torch.Tensor, 
+                              data_groups: List[int], 
+                              beta: float):
+    """Utils to update a DictDataset object with the appropriate weights and labels for the training set for the learning of committor function.
 
     Parameters
     ----------
-    dataframe : pd.Dataframe
-        Pandas dataframe with the training data, including descriptors, labels and bias columns.
-        This should be created using the mlcolvar.utils.io.create_dataset_from_files function
-    dataset : _type_
-        Dataset to be updated. This should be created using the mlcolvar.utils.io.create_dataset_from_files function
+    dataset : 
+        Labeled dataset containig data from different simulations, the labels must identify each of them. 
+        For example, it can be created using `mlcolvar.utils.io.create_dataset_from_files(filenames=[file1, ..., fileN], ... , create_labels=True)`
+    bias : torch.Tensor
+        Bias values for the data in the dataset, usually it should be the committor-based bias
+    data_groups : List[int]
+        Indices specyfing the iteration each labeled data group belongs to. 
+        Unbiased simulations in A and B used for the boundary conditions must have indices 0 and 1.
     beta : float
         Inverse temperature in the right energy units
 
     Returns
     -------
-    Dataframe
-        Updated Pandas dataframe with the 'weights' column
-    Dataset
-        Updated dataset with the 'weights' entry
+        Updated dataset with weights and updated labels
     """
 
-    # TODO make this more general
+    if bias.isnan().any():
+        raise(ValueError('Found Nan(s) in bias tensor. Check before proceeding! If no bias was applied replace Nan with zero!'))
 
-    # Check if we have the bias column and sanitize it
-    if 'bias' in dataframe.columns:
-        dataframe = dataframe.fillna({'bias': 0})
-    else:
-        dataframe['bias'] = 0
+    # TODO sign if not from committor bias
+    weights = torch.exp(beta * bias)
+    new_labels = torch.zeros_like(dataset['labels'])
 
-    # compute weights
-    dataframe['weights'] = np.exp(beta * dataframe['bias'])
+    data_groups = torch.Tensor(data_groups)
 
-    # group data from same iteration under the same label, keep 0 and 1 safe because unbiased!
-    dataframe.loc[np.logical_and(dataframe['labels'] % 2 == 1 , dataframe['labels'] > 1), 'labels'] = dataframe.loc[np.logical_and(dataframe['labels'] % 2 == 1 , dataframe['labels'] > 1), 'labels'] - 1
-    dataframe.loc[dataframe['labels'] > 1, 'labels'] = dataframe.loc[dataframe['labels'] > 1, 'labels'] / 2 + 1
+    # correct data labels according to iteration
+    for j,index in enumerate(data_groups):
+        new_labels[torch.nonzero(dataset['labels'] == j, as_tuple=True)] = index
 
-    # get the reweight averages
-    for i in np.unique(dataframe['labels'].values):
+    for i in np.unique(data_groups):
         # compute average of exp(beta*V) on this simualtions
-        coeff = 1 / np.mean(dataframe.loc[dataframe['labels'] == i, 'weights'].values)
-    
+        coeff = 1 / torch.mean(weights[torch.nonzero(new_labels == i, as_tuple=True)])
+        
         # update the weights
-        dataframe.loc[dataframe['labels'] == i, 'weights'] = coeff * dataframe.loc[dataframe['labels'] == i, 'weights']
+        weights[torch.nonzero(new_labels == i, as_tuple=True)] = coeff * weights[torch.nonzero(new_labels == i, as_tuple=True)]
     
-    # update labels add weights to torch dataset
-    dataset['labels'] = torch.Tensor(dataframe['labels'].values)
-    dataset['weights'] = torch.Tensor(dataframe['weights'].values)
-    
-    return dataframe, dataset
+    # update dataset
+    dataset['weights'] = weights
+    dataset['labels'] = new_labels
+
+    return dataset
 
 def initialize_committor_masses(atoms_map : list, n_dims : int = 3):
     """Initialize the masses tensor with the right shape for committor learning
