c501e8e5bf8c7062dcdad1d348244584d9f58bca
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-06-13 22:24:24 +0200

    Set default dtype.

--- mlcolvar/graph/__init__.py
+++ mlcolvar/graph/__init__.py
@@ -1,7 +1,9 @@
 """The graph neural network module."""
 
 from . import utils
-from . import core 
-from . import data 
+from . import core
+from . import data
 from . import cvs
 from . import explain
+
+utils.torch_tools.set_default_dtype('float32')
