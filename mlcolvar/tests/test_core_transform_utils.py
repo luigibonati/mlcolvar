488cbc0a6bbefb5f602412f9e216fcb645b56cef
EnricoTrizio <enricotrizio@gmail.com>
2024-06-14 13:57:14 +0200

    Added Sequentialtransform helper to fix Multiple preprocessing transforms wrapped in sequential failing in training #140

--- mlcolvar/tests/test_core_transform_utils.py
+++ mlcolvar/tests/test_core_transform_utils.py
@@ -1,5 +1,6 @@
-from mlcolvar.core.transform.utils import test_inverse, test_statistics
+from mlcolvar.core.transform.utils import test_inverse, test_statistics, test_sequential_transform
 
 if __name__ == "__main__":
     test_inverse()
     test_statistics()
+    test_sequential_transform()
