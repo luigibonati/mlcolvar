fc5d69295c443a44ace6e6386dc96e127aa1b827
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-07-07 22:34:49 +0200

    Modified default analysis method.

--- mlcolvar/graph/explain/sensitivity.py
+++ mlcolvar/graph/explain/sensitivity.py
@@ -7,16 +7,16 @@ from mlcolvar.graph import cvs as gcvs
 from mlcolvar.graph import data as gdata
 from mlcolvar.graph import utils as gutils
 
-from .utils import get_dataset_cv_values
+from .utils import get_dataset_cv_values, get_dataset_cv_gradients
 
 """
 Sensitivity analysis.
 """
 
-__all__ = ['graph_node_sensitivity']
+__all__ = ['graph_node_sensitivity', 'graph_node_sensitivity_mask']
 
 
-def graph_node_sensitivity(
+def graph_node_sensitivity_mask(
     model: gcvs.GraphBaseCV,
     dataset: gdata.GraphDataSet,
     node_indices: List[int] = None,
@@ -128,5 +128,62 @@ def graph_node_sensitivity(
     results['sensitivities_components'] = np.array(
         sensitivities_components
     ).transpose(2, 1, 0)
+
+    return results
+
+
+def graph_node_sensitivity(
+    model: gcvs.GraphBaseCV,
+    dataset: gdata.GraphDataSet,
+    component: int = 0,
+    device: str = 'cpu',
+    batch_size: int = None,
+    show_progress: bool = True
+) -> Dict[str, np.ndarray]:
+    """
+    Perform a sensitivity analysis by calculating CV gradient w.r.t. nodes'
+    positions. This allows us to measure which atom is most important to the
+    CV.
+
+    Parameters
+    ----------
+    model: mlcolvar.graph.cvs.GraphBaseCV
+        Collective variable model.
+    dataset: mlcovar.graph.data.GraphDataSet
+        Dataset on which to compute the sensitivity analysis.
+    device: str
+        Name of the device.
+    batch_size:
+        Batch size used for evaluating the CV.
+    show_progress: bool
+        If show the progress bar.
+
+    Returns
+    -------
+    results: dictionary
+        Results of the sensitivity analysis, containing 'node_indices',
+        'sensitivities', and 'sensitivities_components', ordered according to
+        the node indices.
+
+    See also
+    --------
+    mlcolvar.utils.explain.sensitivity_analysis
+        Perform the sensitivity analysis of a feedforward model.
+    """
+    model = model.to(device)
+
+    gradients = get_dataset_cv_gradients(
+        model,
+        dataset,
+        component,
+        batch_size,
+        show_progress,
+        'Getting gradients'
+    )
+    sensitivities_components = np.linalg.norm(gradients, axis=-1)
+
+    results = {}
+    results['sensitivities'] = sensitivities_components.mean(axis=0)
+    results['sensitivities_components'] = sensitivities_components
 
     return results
--- mlcolvar/graph/explain/utils.py
+++ mlcolvar/graph/explain/utils.py
@@ -1,12 +1,11 @@
 import torch
 import numpy as np
-from typing import Dict
 
 from mlcolvar.graph import cvs as gcvs
 from mlcolvar.graph import data as gdata
 from mlcolvar.graph import utils as gutils
 
-__all__ = ['get_dataset_cv_values']
+__all__ = ['get_dataset_cv_values', 'get_dataset_cv_gradients']
 
 """
 Analysis utils.
@@ -19,7 +18,7 @@ def get_dataset_cv_values(
     batch_size: int = None,
     show_progress: bool = True,
     progress_prefix: str = 'Calculating CV values'
-) -> Dict[str, np.ndarray]:
+) -> np.ndarray:
     """
     Get CV values of a given dataset. The calculation will run on the device
     where the model is on.
@@ -63,3 +62,71 @@ def get_dataset_cv_values(
             cv_values.append(outputs)
 
     return np.concatenate(cv_values)
+
+
+def get_dataset_cv_gradients(
+    model: gcvs.GraphBaseCV,
+    dataset: gdata.GraphDataSet,
+    component: int = 0,
+    batch_size: int = None,
+    show_progress: bool = True,
+    progress_prefix: str = 'Calculating CV gradients'
+) -> np.ndarray:
+    """
+    Get gradients of the CV w.r.t. node positions in a given dataset. The
+    calculation will run on the device where the model is on.
+
+    Parameters
+    ----------
+    model: mlcolvar.graph.cvs.GraphBaseCV
+        Collective variable model.
+    dataset: mlcovar.graph.data.GraphDataSet
+        Dataset on which to compute the sensitivity analysis.
+    component: int
+        Component of the CV to analysis.
+    batch_size:
+        Batch size used for evaluating the CV.
+    show_progress: bool
+        If show the progress bar.
+    """
+    datamodule = gdata.GraphDataModule(
+        dataset,
+        lengths=(1.0,),
+        batch_size=batch_size,
+        random_split=False,
+        shuffle=False
+    )
+    datamodule.setup()
+
+    cv_value_gradients = []
+    device = next(model.parameters()).device
+
+    if show_progress:
+        items = gutils.progress.pbar(
+            datamodule.train_dataloader(),
+            frequency=0.001,
+            prefix=progress_prefix
+        )
+    else:
+        items = datamodule.train_dataloader()
+
+    for batchs in items:
+        batch_dict = batchs.to(device).to_dict()
+        cv_values = model(batch_dict)
+        cv_values = cv_values[:, component]
+        grad_outputs = [torch.ones_like(cv_values, device=device)]
+        gradients = torch.autograd.grad(
+            outputs=[cv_values],
+            inputs=[batch_dict['positions']],
+            grad_outputs=grad_outputs,
+            retain_graph=False,
+            create_graph=False,
+        )
+        graph_sizes = batch_dict['ptr'][1:] - batch_dict['ptr'][:-1]
+        gradients = torch.split(
+            gradients[0].detach(), graph_sizes.cpu().numpy().tolist()
+        )
+        gradients = [g.cpu().numpy() for g in gradients]
+        cv_value_gradients.extend(gradients)
+
+    return np.array(cv_value_gradients)
