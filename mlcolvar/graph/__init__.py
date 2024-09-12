21d807a6cbce9229b85391427b632a2d259bf808
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-09-12 14:23:12 +0200

    Do not compile `torch_scatter`.

--- mlcolvar/graph/__init__.py
+++ mlcolvar/graph/__init__.py
@@ -7,3 +7,5 @@ from . import cvs
 from . import explain
 
 utils.torch_tools.set_default_dtype('float32')
+# torch_scatter will compile problems.
+__import__('torch_geometric').typing.WITH_TORCH_SCATTER = False
