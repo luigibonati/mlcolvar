3311f3f6d9fae274eb37d1000f8e0213c1ff6dc0
EnricoTrizio <enricotrizio@gmail.com>
2024-12-16 11:49:16 +0100

    Improved tests

--- mlcolvar/tests/test_cvs_supervised_tda.py
+++ mlcolvar/tests/test_cvs_supervised_tda.py
@@ -1,4 +1,6 @@
 from mlcolvar.cvs.supervised.deeptda import test_deeptda_cv
+from mlcolvar.core.loss.tda_loss import test_tda_loss 
 
 if __name__ == "__main__":
     test_deeptda_cv()
+    test_tda_loss()
---
+++ mlcolvar/tests/test_data_graph.py
@@ -0,0 +1,6 @@
+from mlcolvar.data.graph.atomic import test_atomic_number_table
+from mlcolvar.data.graph.neighborhood import test_get_neighborhood
+
+if __name__ == '__main__':
+    test_atomic_number_table()
+    test_get_neighborhood()
