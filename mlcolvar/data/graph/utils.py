4ab8763291c5e47644b68db46f83108e3c567e06
EnricoTrizio <enricotrizio@gmail.com>
2026-01-28 18:42:28 +0100

    Refactoring

--- mlcolvar/data/graph/utils.py
+++ mlcolvar/data/graph/utils.py
@@ -16,7 +16,7 @@ from typing import List
 
 __all__ = ["create_dataset_from_configurations", "create_test_graph_input"]
 
-def _create_dataset_from_configuration(
+def _create_pyg_data_from_configuration(
     config: atomic.Configuration,
     z_table: atomic.AtomicNumberTable,
     cutoff: float,
@@ -44,96 +44,68 @@ def _create_dataset_from_configuration(
     # the graph, like, we don't even change the node indices in `edge_index`.
     # Here we simply ignore them, and rely on the `RemoveIsolatedNodes` method
     # that will be called later (in `create_dataset_from_configurations`).
-    edge_index, shifts, unit_shifts = get_neighborhood(
-        positions=config.positions,
-        cutoff=cutoff,
-        cell=config.cell,
-        pbc=config.pbc,
-        system_indices=config.system,
-        environment_indices=config.environment,
-        buffer=buffer
-    )
-    edge_index = torch.tensor(edge_index, dtype=torch.long)
-    shifts = torch.tensor(shifts, dtype=torch.get_default_dtype())
-    unit_shifts = torch.tensor(
-        unit_shifts, dtype=torch.get_default_dtype()
-    )
+    edge_index, shifts, unit_shifts = get_neighborhood(positions=config.positions,
+                                                       cutoff=cutoff,
+                                                       cell=config.cell,
+                                                       pbc=config.pbc,
+                                                       system_indices=config.system,
+                                                       environment_indices=config.environment,
+                                                       buffer=buffer
+                                                    )
+    
+    edge_index  = torch.tensor( edge_index, dtype=torch.long )
+    shifts      = torch.tensor( shifts, dtype=torch.get_default_dtype() )
+    unit_shifts = torch.tensor( unit_shifts, dtype=torch.get_default_dtype() )
+    positions   = torch.tensor( config.positions, dtype=torch.get_default_dtype() )
+    cell        = torch.tensor( config.cell, dtype=torch.get_default_dtype() )
+    
+    
+    node_labels  = torch.tensor( config.node_labels, dtype=torch.get_default_dtype() )    if config.node_labels is not None else None
+    graph_labels = torch.tensor( config.graph_labels, dtype=torch.get_default_dtype() )   if config.graph_labels is not None else None
+    weight       = torch.tensor( config.weight, dtype=torch.get_default_dtype() )         if config.weight is not None else 1
 
-    positions = torch.tensor(
-        config.positions, dtype=torch.get_default_dtype()
-    )
-    cell = torch.tensor(config.cell, dtype=torch.get_default_dtype())
-
+    # get indices from atomic numbers and convert to one_hot
     indices = z_table.zs_to_indices(config.atomic_numbers)
-    one_hot = to_one_hot(
-        torch.tensor(indices, dtype=torch.long).unsqueeze(-1),
-        n_classes=len(z_table),
-    )
+    one_hot = to_one_hot( torch.tensor( indices, dtype=torch.long ).unsqueeze(-1), n_classes=len(z_table) )
 
-    node_labels = (
-        torch.tensor(config.node_labels, dtype=torch.get_default_dtype())
-        if config.node_labels is not None
-        else None
-    )
-
-    graph_labels = (
-        torch.tensor(config.graph_labels, dtype=torch.get_default_dtype())
-        if config.graph_labels is not None
-        else None
-    )
-
-    weight = (
-        torch.tensor(config.weight, dtype=torch.get_default_dtype())
-        if config.weight is not None
-        else 1
-    )
-
-    n_system = (
-        torch.tensor(
-            [[len(config.system)]], dtype=torch.get_default_dtype()
-        ) if config.system is not None
-        else torch.tensor(
-            [[one_hot.shape[0]]], dtype=torch.get_default_dtype()
-        )
-    )
-
-    n_env = (
-        torch.tensor(
-            [[one_hot.shape[0] - n_system.to(torch.int).item()]], dtype=torch.get_default_dtype()
-        )
-    )
-
+    # set n_system and system_mask
     if config.system is not None:
+        n_system     = torch.tensor( [[len(config.system)]], dtype=torch.get_default_dtype() )
         system_masks = torch.zeros((one_hot.shape[0], 1), dtype=torch.bool)
         system_masks[config.system, 0] = 1
     else:
+        n_system     = torch.tensor( [[one_hot.shape[0]]], dtype=torch.get_default_dtype() )
         system_masks = None
 
-    return torch_geometric.data.Data(
-        edge_index=edge_index,
-        shifts=shifts,
-        unit_shifts=unit_shifts,
-        positions=positions,
-        cell=cell,
-        node_attrs=one_hot,
-        node_labels=node_labels,
-        graph_labels=graph_labels,
-        n_system=n_system,
-        n_env=n_env,
-        system_masks=system_masks,
-        weight=weight,
-    )
+    # set n_env
+    n_env   = torch.tensor( [[one_hot.shape[0] - n_system.to(torch.int).item()]], dtype=torch.get_default_dtype() )
 
 
-def create_dataset_from_configurations(
-    config: atomic.Configurations,
-    z_table: atomic.AtomicNumberTable,
-    cutoff: float,
-    buffer: float = 0.0,
-    atom_names: List = None,
-    remove_isolated_nodes: bool = False,
-    show_progress: bool = True
-) -> DictDataset:
+    pyg_data = torch_geometric.data.Data(edge_index=edge_index,
+                                         shifts=shifts,
+                                         unit_shifts=unit_shifts,
+                                         positions=positions,
+                                         cell=cell,
+                                         node_attrs=one_hot,
+                                         node_labels=node_labels,
+                                         graph_labels=graph_labels,
+                                         n_system=n_system,
+                                         n_env=n_env,
+                                         system_masks=system_masks,
+                                         weight=weight,
+                                        )
+    
+    return pyg_data
+
+
+def create_dataset_from_configurations(config: atomic.Configurations,
+                                       z_table: atomic.AtomicNumberTable,
+                                       cutoff: float,
+                                       buffer: float = 0.0,
+                                       atom_names: List = None,
+                                       remove_isolated_nodes: bool = False,
+                                       show_progress: bool = True
+                                      ) -> DictDataset:
     """Build DictDataset object containing torch_geometric graph data objects from configurations.
 
     Parameters
@@ -158,30 +130,30 @@ def create_dataset_from_configurations(
     else:
         items = config
     
-    data_list = [
-        _create_dataset_from_configuration(
-            config=c, 
-            z_table=z_table, 
-            cutoff=cutoff, 
-            buffer=buffer, 
-        ) for c in items
-    ]
+    # create a list of torch_geometric data objects, one for each configuration
+    data_list = [ _create_pyg_data_from_configuration(config=c, 
+                                                      z_table=z_table, 
+                                                      cutoff=cutoff, 
+                                                      buffer=buffer, 
+                                                     ) for c in items
+                ]
 
+    # get atom names if needed
     if atom_names is None:
         atom_names_system = [f"X{i}" for i in range(data_list[0]['n_system'].to(torch.int64).item())]
-        atom_names_env = [f"Y{i}" for i in range(data_list[0]['n_env'].to(torch.int64).item())]
-        atom_names = atom_names_system + atom_names_env
+        atom_names_env    = [f"Y{i}" for i in range(data_list[0]['n_env'].to(torch.int64).item())]
+        atom_names        = atom_names_system + atom_names_env
 
-    # this is only to check what isolated nodes have been removed
-    _aux_pos = torch.Tensor((np.array([d['positions'].numpy() for d in data_list])))
+
     if remove_isolated_nodes:
-        # TODO: not the worst way to fake the `is_node_attr` method of
-        # `torch_geometric.data.storage.GlobalStorage` ...
-        # I mean, when there are exact three atoms in the graph, the
-        # `RemoveIsolatedNodes` method will remove the cell vectors that
-        # correspond to the isolated node ... This is a consequence of that
-        # pyg regarding the cell vectors as some kind of node features.
-        # So here we first remove the isolated nodes, then set the cell back.
+        # TODO: not the worst way to fake the `is_node_attr` method of `torch_geometric.data.storage.GlobalStorage`
+        # If there are exact three atoms in the graph, the `RemoveIsolatedNodes` method will remove the cell vectors that
+        # correspond to the isolated node. This is a consequence of pyg regarding the cell vectors as some kind of node features.
+        # So here we first have to remove the isolated nodes and then set the cell back.
+        
+        # this aux var is only to check what isolated nodes have been removed
+        _aux_pos = torch.Tensor((np.array([d['positions'].numpy() for d in data_list])))
+        
         cell_list = [d.cell.clone() for d in data_list]
         transform = _RemoveIsolatedNodes()
         data_list = [transform(d) for d in data_list]
@@ -190,11 +162,13 @@ def create_dataset_from_configurations(
         unique_idx = [] # store the indeces of the atoms that have been used at least once
         for i in range(len(data_list)):
             data_list[i].cell = cell_list[i]
+
             # get and save the original index before removing isolated nodes for each entry
             original_idx = torch.unique( torch.where(torch.isin(torch.round(_aux_pos[i], decimals=5), 
                                                                 torch.round(data_list[i]['positions'], decimals=5))
                                                     )[0]
                                         )
+            
             data_list[i]['names_idx'] = original_idx.to(torch.int64)
             
             # update if needed the overall list
@@ -205,7 +179,8 @@ def create_dataset_from_configurations(
         
         unique_idx.sort()
         unique_idx = torch.Tensor(unique_idx).to(torch.int64)
-    # here we simply have to take all the atoms
+    
+    # if not remove_isolated_nodes we simply take all the atoms
     else:
         unique_idx = torch.arange(data_list[0]['n_system'].item()).to(torch.int64)
         for i in range(len(data_list)):
@@ -215,14 +190,15 @@ def create_dataset_from_configurations(
     unique_names = np.array(atom_names)[unique_idx]
     unique_names = unique_names.tolist()
 
-    dataset = DictDataset(dictionary={'data_list' : data_list},
-                          metadata={'z_table' : z_table.zs,
-                                    'cutoff' : cutoff,
-                                    'used_idx' : unique_idx,
-                                    'used_names' : unique_names},
+    dataset = DictDataset(dictionary={'data_list': data_list},
+                          metadata={'z_table': z_table.zs,
+                                    'cutoff': cutoff,
+                                    'used_idx': unique_idx,
+                                    'used_names': unique_names},
                           data_type='graphs')
 
     return dataset
+
 
 def to_one_hot(indices: torch.Tensor, n_classes: int) -> torch.Tensor:
     """Generates one-hot encoding with `n_classes` classes from `indices`
@@ -240,22 +216,33 @@ def to_one_hot(indices: torch.Tensor, n_classes: int) -> torch.Tensor:
         The one-hot encoding
     """
     shape = indices.shape[:-1] + (n_classes,)
-    oh = torch.zeros(shape, device=indices.device).view(shape)
+    one_hot = torch.zeros(shape, device=indices.device).view(shape)
 
     # scatter_ is the in-place version of scatter
-    oh.scatter_(dim=-1, index=indices, value=1)
+    one_hot.scatter_(dim=-1, index=indices, value=1)
 
-    return oh.view(*shape)
+    return one_hot.view(*shape)
 
 
 class _RemoveIsolatedNodes(BaseTransform):
     r"""Removes isolated nodes from the graph
     This is taken from pytorch_geometric with a small modification to avoid the bug when n_nodes==n_edges
     """
-    def forward(
-        self,
-        data: Union[Data, HeteroData],
-    ) -> Union[Data, HeteroData]:
+    def forward(self,
+                data: Union[Data, HeteroData],
+               ) -> Union[Data, HeteroData]:
+        """Remove isolated nodes from graphs in a pytorch_geometric Data object
+
+        Parameters
+        ----------
+        data : Union[Data, HeteroData]
+            Pytorch_geometric Data object containing the graph data
+
+        Returns
+        -------
+        Union[Data, HeteroData]
+            Pytorch_geometric Data object containing the modified graph data
+        """
         # Gather all nodes that occur in at least one edge (across all types):
         n_ids_dict = defaultdict(list)
         for edge_store in data.edge_stores:
@@ -545,7 +532,7 @@ def test_from_configuration() -> None:
     )
 
     # create dataset from a configuration
-    data = _create_dataset_from_configuration(config, z_table, 0.1)
+    data = _create_pyg_data_from_configuration(config, z_table, 0.1)
 
     # check edges and shifts are created correctly
     assert(data['edge_index'] == torch.tensor([[0, 0, 1, 1, 2, 2],
@@ -604,7 +591,7 @@ def test_from_configuration() -> None:
         environment=[2]
     )
     
-    data = _create_dataset_from_configuration(config, z_table, 0.1)
+    data = _create_pyg_data_from_configuration(config, z_table, 0.1)
     
     # check edges and shift are computed correctly
     assert(data['edge_index'] == torch.tensor([[1, 2],
@@ -629,7 +616,7 @@ def test_from_configuration() -> None:
         environment=[1, 2]
     )
 
-    data = _create_dataset_from_configuration(config, z_table, 0.1)
+    data = _create_pyg_data_from_configuration(config, z_table, 0.1)
     assert(data['edge_index'] == torch.tensor([[0, 0, 1, 1, 2, 2], 
                                                 [2, 1, 0, 2, 1, 0]])
             ).all()
@@ -653,7 +640,7 @@ def test_from_configuration() -> None:
         environment=[1, 2]
     )
     # create dataset with same cutoff
-    data = _create_dataset_from_configuration(config, z_table, 0.1)
+    data = _create_pyg_data_from_configuration(config, z_table, 0.1)
     
     # check third atom is not included anymore
     assert (data['edge_index'] == torch.tensor([[0, 1], 
@@ -661,7 +648,7 @@ def test_from_configuration() -> None:
         ).all()
 
     # create dataset with slightly large cutoff
-    data = _create_dataset_from_configuration(config, z_table, 0.11)
+    data = _create_pyg_data_from_configuration(config, z_table, 0.11)
     
     # check the edge with the third atom is created once again
     assert(data['edge_index'] == torch.tensor([[0, 0, 1, 1, 2, 2], 
@@ -670,7 +657,7 @@ def test_from_configuration() -> None:
     
     # check with buffer layer
     # the third atoms should be included but with no edge to the system atom
-    data = _create_dataset_from_configuration(config, z_table, 0.1, 0.01)
+    data = _create_pyg_data_from_configuration(config, z_table, 0.1, 0.01)
     assert(data['edge_index'] == torch.tensor([[0, 1, 1, 2], 
                                                 [1, 0, 2, 1]])
             ).all()
