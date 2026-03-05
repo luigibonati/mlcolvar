c09fc528b87c64afb2590eb50d06b4526269d778
EnricoTrizio <enricotrizio@gmail.com>
2026-03-05 12:18:08 +0100

    Fixed_test

--- mlcolvar/core/loss/utils/smart_derivatives.py
+++ mlcolvar/core/loss/utils/smart_derivatives.py
@@ -1181,7 +1181,7 @@ def test_train_with_smart_derivatives():
         eta=0.005,
         alpha=0.01,
         friction=friction,
-        cell=None,
+        rescaling_cell=None,
         descriptors_derivatives=smart_derivatives,
         options=options,
     )
