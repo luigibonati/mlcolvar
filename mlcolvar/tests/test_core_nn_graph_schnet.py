13b233a274f581c58077cf656234b19ca817e760
EnricoTrizio <enricotrizio@gmail.com>
2026-04-13 17:40:53 +0200

    Added tests

--- mlcolvar/tests/test_core_nn_graph_schnet.py
+++ mlcolvar/tests/test_core_nn_graph_schnet.py
@@ -1,5 +1,6 @@
-from mlcolvar.core.nn.graph.schnet import test_schnet_1, test_schnet_2
+from mlcolvar.core.nn.graph.schnet import test_schnet_1, test_schnet_2, test_schnet_from_dataset
 
 if __name__ == "__main__":
     test_schnet_1()
     test_schnet_2()
+    test_schnet_from_dataset()
