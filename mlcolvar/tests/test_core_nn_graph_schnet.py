8ff4ab91a6cf24863c143feee1ccfeb22e2b1237
Kai Zhu <22319143@zju.edu.cn>
2026-04-14 16:15:37 +0200

    test_schnet_4()

--- mlcolvar/tests/test_core_nn_graph_schnet.py
+++ mlcolvar/tests/test_core_nn_graph_schnet.py
@@ -1,6 +1,7 @@
-from mlcolvar.core.nn.graph.schnet import test_schnet_1, test_schnet_2, test_schnet_3
+from mlcolvar.core.nn.graph.schnet import test_schnet_1, test_schnet_2, test_schnet_3, test_schnet_4
 
 if __name__ == "__main__":
     test_schnet_1()
     test_schnet_2()
     test_schnet_3()
+    test_schnet_4()
