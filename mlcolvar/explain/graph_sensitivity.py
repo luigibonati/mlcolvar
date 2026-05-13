a1377935282558eae02b05f3c04a4a8a2a557f4c
EnricoTrizio <enricotrizio@gmail.com>
2026-05-13 11:35:57 +0200

    Add save sensitivity to extxyz

--- mlcolvar/explain/graph_sensitivity.py
+++ mlcolvar/explain/graph_sensitivity.py
@@ -1,23 +1,25 @@
 import numpy as np
 from typing import Dict
 import torch
+import copy
 
 from mlcolvar.data import DictModule
 from mlcolvar.utils.plot import pbar
 from mlcolvar.core.nn import BaseGNN
+from mlcolvar.data.utils import save_dataset_configurations_as_extyz
 
 
 __all__ = ['graph_node_sensitivity']
 
 
-def graph_node_sensitivity(
-    model,
-    dataset,
-    component: int = 0,
-    device: str = 'cpu',
-    batch_size: int = None,
-    show_progress: bool = True
-) -> Dict[str, np.ndarray]:
+def graph_node_sensitivity(model,
+                           dataset,
+                           component: int = 0,
+                           device: str = 'cpu',
+                           batch_size: int = None,
+                           show_progress: bool = True,
+                           extxyz_filename: str = None,
+                          ) -> Dict[str, np.ndarray]:
     """Performs a sensitivity analysis on a GNN-based CV model using
     partial derivatives w.r.t. nodes' positions. 
     This allows us to measure which atom is most important to the CV model.
@@ -25,7 +27,8 @@ def graph_node_sensitivity(
     If system/environment atoms are defined in the input dataset, the average node-sensitivities are returned only
     for the system atom, while a aggregated sensitivities (mean and sum) are returned for environment instead.
     
-    # TODO xyz?
+    Optionally, the results can be printed to an extxyz, where each atom is associated to its sensitivity score
+    so that they can be visualized in a molecular viewer.
 
     Parameters
     ----------
@@ -39,6 +42,9 @@ def graph_node_sensitivity(
         Batch size used for evaluating the CV
     show_progress: bool
         If to show the progress bar
+    extxyz_filename: str
+        If provided, a extxyz file with this name is printed with the positions of the atoms in the graph dataset 
+        with the corresponding sensitivities so that they can be visualized in a molecular viewer.
 
     Returns
     -------
@@ -47,11 +53,11 @@ def graph_node_sensitivity(
           - 'node_labels': names associated to the nodes of the graphs. If truncated graphs are used, only the system atoms
                            are labeled, while the contribution from the environment atoms is aggregated with mean and sum.
           - 'avg_sensitivities': averaged sensititivities over the given dataset. If truncated graphs are used, environment 
-                                 values are aggregated with mean and sum.
+                                 values are aggregated with mean and sum. The results are ordered consistently with the node
+                                 labels, the enviroment values are the last two.
           - 'raw_sensitivities': raw sensitivities per-frame, including the sensitivities relative to each atom. If truncated
                                  graphs are used, the sensitivities wrt different environment atoms (whose number may change
-                                 for different frame) are returned.
-        The quantities are ordered consistently with the node labels.
+                                 for different frame) are returned. The results are ordered as the positions in the dataset.
 
     See also
     --------
@@ -66,7 +72,7 @@ def graph_node_sensitivity(
 
     # make user aware of behaviour for truncated graphs
     if dataset.metadata['is_truncated_graph']:
-        print("The input dataset contains truncated graphs for which system and environment selections are provided. the average node-sensitivities" +
+        print("[NOTE] The input dataset contains truncated graphs for which system and environment selections are provided.\nThe average node-sensitivities" +
              "will be returned only for the system atom, while an aggregated sensitivty is returned for environment instead")
 
     model = model.to(device)
@@ -92,7 +98,15 @@ def graph_node_sensitivity(
     results['avg_sensitivities'] = gradients_norm
     results['raw_sensitivities'] = gradients_components
     
-
+    # save an extxyz file with the atoms in the graph and their sensitivity score
+    if extxyz_filename is not None:
+        updated_dataset = copy.deepcopy(dataset)
+        for i, d in enumerate(updated_dataset['data_list']):
+            d['sensitivities'] = gradients_components[i]
+        save_dataset_configurations_as_extyz(dataset=updated_dataset,
+                                             file_name=extxyz_filename,
+                                             extra_keys=['sensitivities'])
+    
     return results
 
 
@@ -146,14 +160,13 @@ def get_dataset_cv_values(model,
     return np.concatenate(cv_values)
 
 
-def get_dataset_cv_gradients(
-    model,
-    dataset,
-    component: int = 0,
-    batch_size: int = None,
-    show_progress: bool = True,
-    progress_prefix: str = 'Calculating CV gradients'
-) -> np.ndarray:
+def get_dataset_cv_gradients(model,
+                             dataset,
+                             component: int = 0,
+                             batch_size: int = None,
+                             show_progress: bool = True,
+                             progress_prefix: str = 'Calculating CV gradients'
+                            ) -> np.ndarray:
     """Get gradients of a GNN-based CV w.r.t. node positions in a given dataset. 
     The calculation will run on the device where the model is on.
 
@@ -226,19 +239,17 @@ def get_dataset_cv_gradients(
             # system grads are treated individually, env grads are aggragated with sum and mean
             for i,g in enumerate(gradients):
                 # slice the gradients to get the contribution from system and environment atoms
-                system_atoms_grads = g[batch_dict[i]['system_masks'].squeeze()]
-                env_atoms_grads    = g[torch.logical_not(batch_dict[i]['system_masks'].squeeze())]
+                system_atoms_grads = torch.linalg.vector_norm( g[batch_dict[i]['system_masks'].squeeze()], dim=-1)
+                env_atoms_grads    = torch.linalg.vector_norm( g[torch.logical_not(batch_dict[i]['system_masks'].squeeze())], dim=-1)
 
                 # add the values to the result lists
-                # one with the components of the gradients of all atoms, system atoms first
-                cv_value_gradients_components.append(torch.vstack([system_atoms_grads, 
-                                                                   env_atoms_grads
-                                                                  ]).cpu().numpy())
+                # one with the components of the gradients of all atoms, ordered as the positions in the dataset
+                cv_value_gradients_components.append(torch.linalg.vector_norm( g, dim=-1).unsqueeze(-1))    
 
                 # one with the norm of the gradients for each system atom and aggregated env atoms
-                cv_value_gradients.extend(torch.concat([torch.linalg.vector_norm(system_atoms_grads, dim=-1),
-                                                        torch.linalg.vector_norm(env_atoms_grads, dim=-1).mean(dim=-1, keepdim=True), 
-                                                        torch.linalg.vector_norm(env_atoms_grads, dim=-1).sum(dim=-1, keepdim=True)
+                cv_value_gradients.extend(torch.concat([system_atoms_grads,
+                                                        env_atoms_grads.mean(dim=-1, keepdim=True), 
+                                                        env_atoms_grads.sum(dim=-1, keepdim=True)
                                                         ]).unsqueeze(0).cpu().numpy()
                                         )
         else:
@@ -300,10 +311,8 @@ def test_graph_sensitivity():
     from mlcolvar.data.graph.utils import create_test_graph_input
 
     for environment in [False, True]:
-        print("\n\n\n\n\n", environment)
         # create data, we need the dataset for sensitivity analysis later
         dataset = create_test_graph_input(output_type='dataset', n_samples=100, n_states=2, n_atoms=3, environment=environment)
-        print(dataset)
         datamodule = DictModule(dataset=dataset, lengths=[0.8, 0.2], shuffle=[1, 0])
 
         # create model
@@ -329,6 +338,3 @@ def test_graph_sensitivity():
 
         # print results
         print(test_sensitivity)
-
-if __name__ == '__main__':
-    test_graph_sensitivity()
