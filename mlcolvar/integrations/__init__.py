a095c434ce7150737e14201c1acef00f9f96e9cb
Kai Zhu <22319143@zju.edu.cn>
2026-07-06 13:05:32 +0200

    Create __init__.py for integrations

---
+++ mlcolvar/integrations/__init__.py
@@ -0,0 +1,9 @@
+from .atomistic import AtomisticModel, BaseAtomisticFeaturizer
+from .mace_featurizer import MACEFeaturizer
+
+
+__all__ = [
+    "BaseAtomisticFeaturizer",
+    "AtomisticModel",
+    "MACEFeaturizer",
+]
